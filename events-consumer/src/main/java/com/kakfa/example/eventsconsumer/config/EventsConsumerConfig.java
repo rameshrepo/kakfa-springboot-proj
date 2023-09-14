7c9329ed5151f7631defd64374752aa37eb12f3a
Ramesh <rameshs24@gmail.com>
2023-09-14 22:13:29 +1000

    Added Docs

---
+++ events-consumer/src/main/java/com/kakfa/example/eventsconsumer/config/EventsConsumerConfig.java
@@ -0,0 +1,10 @@
+package com.kakfa.example.eventsconsumer.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+// @EnableKafka - Needed for older versions of kakfa
+@Slf4j
+public class EventsConsumerConfig {
+}
---
+++ events-consumer/src/main/java/com/kakfa/example/eventsconsumer/consumer/EventsConsumer.java
@@ -0,0 +1,22 @@
+package com.kakfa.example.eventsconsumer.consumer;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class EventsConsumer {
+    @KafkaListener(
+            topics = {"library-events"}
+            , autoStartup = "${libraryListener.startup:true}"
+            , groupId = "library-events-listener-group")
+    public void onMessage(ConsumerRecord<Integer, String> consumerRecord) throws JsonProcessingException {
+
+        log.info("ConsumerRecord : {} ", consumerRecord);
+        libraryEventsService.processLibraryEvent(consumerRecord);
+
+    }
+}
